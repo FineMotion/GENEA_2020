1511a8eb470920f359507a6e4df7902ff87f92ac
hawkeoni <iliyadimov@icloud.com>
2020-07-30 22:25:47 +0300

    Add __main__

--- train_seq2seq.py
+++ train_seq2seq.py
@@ -4,6 +4,8 @@ import pytorch_lightning as pl
 import torch
 from torch.nn import MSELoss
 from torch.utils.data import DataLoader
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 
 from src.seq2seq_model import Encoder, Decoder
 from src.dataset import Seq2SeqDataset
@@ -30,16 +32,42 @@ class Seq2SeqSystem(pl.LightningModule):
         loss = self.loss(pred_poses, y)
         return {'loss': loss}
 
+    def validation_step(self, batch, batch_nb):
+        x, y, p = batch
+        pred_poses = self.forward(x, y, p)
+        loss = self.loss(pred_poses, y)
+        return {'loss': loss}
+
+    def validation_epoch_end(self, outputs):
+        d = {"val_loss": 0}
+        for out in outputs:
+            d['val_loss'] += out['loss']
+        return d
+
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.001)
 
     def train_dataloader(self):
         dataset = Seq2SeqDataset(Path("data/Ready").glob("*.npz"), self.previous_poses, self.predicted_poses)
-        loader = DataLoader(dataset, batch_size=50, shuffle=False, collate_fn=dataset.collate_fn)
+        loader = DataLoader(dataset, batch_size=50, shuffle=True, collate_fn=dataset.collate_fn)
         return loader
 
+    def val_dataloader(self):
+        dataset = Seq2SeqDataset(Path("data/Ready/dev").glob("*.npz"), self.previous_poses, self.predicted_poses)
+        loader = DataLoader(dataset, batch_size=50, shuffle=True, collate_fn=dataset.collate_fn)
+        return loader
 
-
-system = Seq2SeqSystem(20, 10)
-trainer = pl.Trainer(gpus=1)
-trainer.fit(system)
+if __name__ == "__main__":
+    system = Seq2SeqSystem(50, 20)
+    checkpoint_callback = ModelCheckpoint(
+        filepath="./seq2seq",
+        save_top_k=1,
+        verbose=True,
+        monitor='val_loss',
+        mode='min',
+        prefix=''
+    )
+    trainer = pl.Trainer(gpus=1, default_root_dir="./seq2seq", checkpoint_callback=checkpoint_callback,
+                         max_epochs=50)
+    trainer.fit(system)
+    trainer.save_checkpoint("./seq2seq_checkpoint")
