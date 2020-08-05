6b34bdff176f95a23e2bbde581262ea4140998d9
hawkeoni <iliyadimov@icloud.com>
2020-08-05 11:06:53 +0300

    Improve normalization

--- DataProcessing/reconstruct_data.py
+++ DataProcessing/reconstruct_data.py
@@ -1,28 +1,27 @@
-from os import listdir
-from os.path import join
 from argparse import ArgumentParser
+
 import numpy as np
-import sys
 
-sys.path.append('../tools')
-from normalization import get_normalization_values, create_motion_array
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('--src')
-    arg_parser.add_argument('--dst')
+    arg_parser.add_argument("--src", help=".npy file with prediction")
+    arg_parser.add_argument(
+        "--mean", default="./mean_pose.npz", help="File with normalization statistics."
+    )
+    arg_parser.add_argument(
+        "--dst",
+        default="./pred_unnormalized.npy",
+        help="File to write denormalized result.",
+    )
     args = arg_parser.parse_args()
 
     data = np.load(args.src)
-
-    data_filenames = listdir('../data/Ready')
-    data_files = [join('../data/Ready', data_filename) for data_filename in data_filenames]
-    train_array = create_motion_array(data_files[1:])
-    max_val, mean_pose = get_normalization_values(train_array)
-
+    mean_pose = np.load(args.mean)
+    max_val = mean_pose["max_val"]
+    mean_pose = mean_pose["mean_pose"]
     eps = 1e-8
     reconstructed = np.multiply(data, max_val[np.newaxis, :] + eps)
     reconstructed = reconstructed + mean_pose[np.newaxis, :]
     print(reconstructed.shape)
-
     np.save(args.dst, reconstructed)
